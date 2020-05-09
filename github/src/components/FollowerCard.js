545af7fc6b1c49485b823446c2573a06a1832742
cknoettg <cknoettg@yahoo.com>
2020-05-09 00:29:14 -0400

    Completed ICE, CCR, and 2 Separate axios calls

---
+++ github/src/components/FollowerCard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class FollowerCard extends React.Component {
+    constructor() {
+        super();
+        this.state={
+            myFollowers: {}
+        };
+    }
+
+}
+
+export default FollowerCard;
---
+++ github/src/components/Followers.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import axios from 'axios';
+
+class Followers extends React.Component {
+    constructor() {
+        super();
+        this.state={
+            followers: []
+        };
+    }
+
+    componentDidMount() {
+        axios.get('https://api.github.com/users/TheTrabin/followers')
+        .then(response => {
+            console.log("User data", response);
+            this.setState({
+                user: response.data
+            });
+        })
+        .catch(err => {
+            console.log("Failed to get user", err);
+        });
+        
+    }
+
+    render() {
+        return (
+            <div className="follower-card">
+                <img src={this.state.avatar_url} alt="me" />
+                <div className="card-info">
+                    <h3 className="name">{this.state.name}</h3>
+                    <p className="username">{this.state.login}</p>
+                    <p>Location: {this.state.location}</p>
+                    <p>Profile: 
+                    <a href={this.state.profile}> {this.state.profile} </a>
+                    </p>
+                    <p>Followers: {this.state.followers}</p>
+                    <p>Following: {this.state.following}</p>
+                    <p>Bio: {this.state.bio}</p>
+                </div>
+            </div>
+        )
+    };
+
+}
+
+export default Followers;
