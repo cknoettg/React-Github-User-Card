eb18472d604ddda80b94f67a0e6df449232e236c
cknoettg <cknoettg@yahoo.com>
2020-05-10 15:12:15 -0400

    Still working on getting followers to display

--- github/src/App.js
+++ github/src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import './App.css';
 import User from './components/User';
 //import UserCard from './components/UserCard';
-//import Followers from './components/Followers';
+import Followers from './components/Followers';
 import FollowerCard from './components/FollowerCard';
 
 function App() {
   return (
     <div className="App">
       <User />
+      <Followers />
       {/* <FollowerCard /> */}
     </div>
   );
--- github/src/components/Followers.js
+++ github/src/components/Followers.js
@@ -12,33 +12,38 @@ class Followers extends React.Component {
     componentDidMount() {
         axios.get('https://api.github.com/users/TheTrabin/followers')
         .then(response => {
-            console.log("User data", response);
+            console.log("Follower data", response.data);
+            console.log("Data for one follower", response.data[0]);
+            console.log("Avatar for one follower", response.data[0].avatar_url);
             this.setState({
                 followers: response.data
             });
         })
         .catch(err => {
-            console.log("Failed to get user", err);
+            console.log("Failed to get follower", err);
         });
         
     }
+
+    //loop through each follower? Outsource this
+    // function to FollowerCard
 
     render() {
         return (
             <div className="follower-card">
                 <h3>Followers</h3>
-                <p>{this.state.followers}</p>
-                <img src={this.state.avatar_url} alt="friend" />
+                <p>{console.log(this.state.followers)}</p>
+                <img src={this.state.followers[0].avatar_url} alt="friend" />
                 <div className="card-info">
-                    <h3 className="name">{this.state.name}</h3>
-                    <p className="username">{this.state.login}</p>
-                    <p>Location: {this.state.location}</p>
+                    {/* <h3 className="name">{this.state.followers[0].name}</h3> */}
+                    {/* <p className="username">{this.state.followers.login}</p>
+                    <p>Location: {this.state.followers.location}</p>
                     <p>Profile: 
-                    <a href={this.state.profile}> {this.state.profile} </a>
+                    <a href={this.state.followers.profile}> {this.state.followers.profile} </a>
                     </p>
-                    <p>Followers: {this.state.followers}</p>
-                    <p>Following: {this.state.following}</p>
-                    <p>Bio: {this.state.bio}</p>
+                    <p>Followers: {this.state.followers.followers}</p>
+                    <p>Following: {this.state.followers.following}</p>
+                    <p>Bio: {this.state.followers.bio}</p> */}
                 </div>
             </div>
         )
